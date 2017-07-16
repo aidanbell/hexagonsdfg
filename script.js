6b2706bdf895237ba083f3c671279f734366b84c
aidanbell <aidanbell0182@gmail.com>
2017-07-16 17:48:35 -0400

    asdf
    asdfc

---
+++ script.js
@@ -0,0 +1,12 @@
+var divBoxes = document.getElementsByClassName("selector")
+
+function selected(item){
+    item.style.backgroundColor = "black";
+    if (i=0, 1!==0,i++) {
+        item.value = 1;
+    }
+}
+function hexagon(item){
+    document.getElementByClass("selector").className = "hexagon"
+}
+document.getElementById("returns").innerHTML = value;
